eac5f5bb7f818eae6216518a8c53cf25f27ab3b5
Turbo Frajer <hyperturbofrajer@gmail.com>
2022-08-17 16:52:38 +0200

    Plně funkční v1

--- IPtestovač.py
+++ IPtestovač.py
@@ -1,7 +1,9 @@
 import re, requests
 from netaddr import *
+from datetime import datetime
 
-url = requests.get("ENTER-URL")
+
+url = requests.get("ZADAT LIST IP ADRES")
 url.encoding = "utf-8"
 
 text = url.text
@@ -9,15 +11,31 @@ found = re.findall(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(?:/\d{1,2}|)',text)
 info = url.text.splitlines()
 
 
-najit = input("Zadat IP adresu: ")
+
+najit = input("Zadat IP adresu: ").strip()
+
 
 
 for x in range(len(found)):
     if IPAddress(najit) in (IPNetwork(found[x])):        
         for y in range(len(info)):
             if found[x] in info[y]:
-                print(info[y])
+                #print(info[y])
+                tim = info[y].split()
+                print(f"Adresa {tim[0]} je blacklisted.")
+                if tim[1] == "0":
+                    print("Perma blacklisted.")
+                    duvoda = " ".join(tim[2:])
+                    print(f"Důvod: {duvoda}")
 
+                if tim[1].isnumeric() == True and tim[2].isnumeric() == True:
+                    print(f"Blacklisted od: {datetime.fromtimestamp(int(tim[2]))}")
+                    bando = int(tim[1])+int(tim[2])
+                    print(f"Blacklisted do: {datetime.fromtimestamp(bando)}")
+                    duvodb = " ".join(tim[3:])
+                    print(f"Důvod: {duvodb}")
 
-
+                else:
+                    print(info[y])
+        
 
