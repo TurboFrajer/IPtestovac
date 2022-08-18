ea512930c88bbb571c6f026236d4134ee4f5c6d0
Turbo Frajer <hyperturbofrajer@gmail.com>
2022-08-18 08:53:09 +0200

    Oprava chyb v1.1

--- IPtestovač.py
+++ IPtestovač.py
@@ -3,7 +3,7 @@ from netaddr import *
 from datetime import datetime
 
 
-url = requests.get("ZADAT LIST IP ADRES")
+url = requests.get("ZADAT-IP-ADRESU")
 url.encoding = "utf-8"
 
 text = url.text
@@ -20,7 +20,6 @@ for x in range(len(found)):
     if IPAddress(najit) in (IPNetwork(found[x])):        
         for y in range(len(info)):
             if found[x] in info[y]:
-                #print(info[y])
                 tim = info[y].split()
                 print(f"Adresa {tim[0]} je blacklisted.")
                 if tim[1] == "0":
@@ -28,7 +27,7 @@ for x in range(len(found)):
                     duvoda = " ".join(tim[2:])
                     print(f"Důvod: {duvoda}")
 
-                if tim[1].isnumeric() == True and tim[2].isnumeric() == True:
+                elif tim[1].isnumeric() == True and tim[2].isnumeric() == True:
                     print(f"Blacklisted od: {datetime.fromtimestamp(int(tim[2]))}")
                     bando = int(tim[1])+int(tim[2])
                     print(f"Blacklisted do: {datetime.fromtimestamp(bando)}")
@@ -37,5 +36,14 @@ for x in range(len(found)):
 
                 else:
                     print(info[y])
-        
+                    break
+   
+
+
+
+
+
+
+
+
 
