7ba7f95303650b7216d37657f9b0a0332de61a12
Turbo Frajer <hyperturbofrajer@gmail.com>
2022-08-18 11:24:20 +0200

    Podpora IPv4 + IPv6, v2

--- IPtestovač.py
+++ IPtestovač.py
@@ -1,23 +1,21 @@
-import re, requests
+import requests
 from netaddr import *
 from datetime import datetime
 
 
-url = requests.get("ZADAT-IP-ADRESU")
-url.encoding = "utf-8"
+url4 = requests.get("IPv4 seznam")
+url6 = requests.get("IPv6 seznam")
+url4.encoding = "utf-8"
+url6.encoding = "utf-8"
 
-text = url.text
-found = re.findall(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(?:/\d{1,2}|)',text)
-info = url.text.splitlines()
-
-
-
+text = url4.text + url6.text
+info = text.splitlines()
+found = [z.split()[0] for z in info]
 najit = input("Zadat IP adresu: ").strip()
 
 
-
 for x in range(len(found)):
-    if IPAddress(najit) in (IPNetwork(found[x])):        
+    if IPNetwork(najit) in IPNetwork(found[x]):        
         for y in range(len(info)):
             if found[x] in info[y]:
                 tim = info[y].split()
@@ -26,24 +24,18 @@ for x in range(len(found)):
                     print("Perma blacklisted.")
                     duvoda = " ".join(tim[2:])
                     print(f"Důvod: {duvoda}")
-
+                    
                 elif tim[1].isnumeric() == True and tim[2].isnumeric() == True:
                     print(f"Blacklisted od: {datetime.fromtimestamp(int(tim[2]))}")
                     bando = int(tim[1])+int(tim[2])
                     print(f"Blacklisted do: {datetime.fromtimestamp(bando)}")
                     duvodb = " ".join(tim[3:])
                     print(f"Důvod: {duvodb}")
-
+                    
                 else:
                     print(info[y])
                     break
-   
-
-
-
-
-
-
-
-
+        break
+else:
+    print("Není blacklisted.")
 
